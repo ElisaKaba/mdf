bc9c2a194ce0ed5c6a13f8dba7ee338eb1623a28
ElisaKaba <elisakabapro@gmail.com>
2026-08-31 00:28:50 +0200

    Configure Strapi images for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,13 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    dangerouslyAllowLocalIP: true,
+    dangerouslyAllowLocalIP:
+      process.env.NODE_ENV === "development",
 
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "mdf-68eh.onrender.com",
         pathname: "/uploads/**",
       },
     ],
